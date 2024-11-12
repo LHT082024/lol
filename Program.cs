21e790f8e76bf633227dc056b0563889677fa6dc
LHT082024 <LHT082024@gmail.com>
2024-11-12 13:34:05 +0100

    example of list and arraylist

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace lol;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList phantom = new ArrayList();
+        phantom.Add("hello");
+        phantom.Add(1);
+        phantom.Add(2.2);
+
+        for (int i = 0;)
+
+
+    }
+}
